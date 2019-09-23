a909791fa4c33438644198f21f36aaa3e1da31d4
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2019-09-23 10:41:49 +0200

    Página de crear nuevo diseño terminada

--- src/app/pages/new-design/new-design.component.ts
+++ src/app/pages/new-design/new-design.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Design } from '../../interfaces/interfaces';
-import { ApiService } from '../../services/api.service';
+import { Router }            from '@angular/router';
+import { Design }            from '../../interfaces/interfaces';
+import { ApiService }        from '../../services/api.service';
+import { DialogService }     from '../../services/dialog.service';
 
 @Component({
   selector: 'mcd-new-design',
@@ -17,14 +19,21 @@ export class NewDesignComponent implements OnInit {
 	} as Design;
 	saveSending: boolean = false;
 
-	constructor(private as: ApiService) {}
+	constructor(private dialog: DialogService, private as: ApiService, private router: Router) {}
 	ngOnInit() {}
 	
 	saveDesign(ev){
 		ev.preventDefault();
 		this.saveSending = true;
 		this.as.newDesign(this.newDesign).subscribe(result => {
-			
+			if (result.status=='ok'){
+				this.dialog.alert({title: 'OK', content: 'New design "'+this.newDesign.name+'" has been saved.', ok: 'Continue'}).subscribe(result => {
+					this.router.navigate(['/main']);
+				});
+			}
+			else{
+				this.dialog.alert({title: 'Error', content: 'There was an error when saving the new design. Please try again later.', ok: 'Continue'}).subscribe(result => {});
+			}
 		});
 	}
 }
